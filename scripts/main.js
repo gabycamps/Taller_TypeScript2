bd9131730f3b8beedf63f23919a5a3bcfd1429f9
gabycamps <g.camposm@uniandes.edu.co>
2025-10-19 20:28:16 -0500

    avance

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ renderSeriesInTable(series);
 function renderSeriesInTable(series) {
     series.forEach(function (s) {
         var trElement = document.createElement("tr");
+        trElement.addEventListener("click", function () { return renderSerieDetail(s); });
         trElement.innerHTML = "<td>".concat(s.id, "</td>\n                            <td>").concat(s.name, "</td>\n                            <td>").concat(s.channel, "</td>\n                            <td>").concat(s.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
@@ -22,5 +23,13 @@ function renderAverageSeasons() {
     avgElement.textContent = "Seasons average: ".concat(average);
     avgElement.classList.add("avg");
     tableContainer.appendChild(avgElement);
+}
+function renderSerieDetail(series) {
+    var cardContainer = document.getElementById("card-container");
+    if (!cardContainer)
+        return;
+    cardContainer.innerHTML = "";
+    var cardHTML = "\n    <div class=\"card text-white bg-dark mb-3\" style=\"max-width: 18rem;>\n        <img src=\"".concat(series.imageUrl, "\" class=\"card-img-top\" alt=\"").concat(series.name, "\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">").concat(series.name, "</h5>\n            <p class=\"card-text\">").concat(series.description, "</p>\n            <a href=\"").concat(series.link, "\" class=\"btn btn-primary\">Go watch it</a>\n        </div>\n    </div>\n    ");
+    cardContainer.innerHTML = cardHTML;
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ function renderSeriesInTable(series: Serie[]): void {
 
     series.forEach(s => {
         let trElement = document.createElement("tr");
+    trElement.addEventListener("click", () => renderSerieDetail(s));
     trElement.innerHTML = `<td>${s.id}</td>
                             <td>${s.name}</td>
                             <td>${s.channel}</td>
@@ -36,4 +37,24 @@ function renderAverageSeasons(): void {
     avgElement.textContent = `Seasons average: ${average}`;
     avgElement.classList.add("avg");
     tableContainer.appendChild(avgElement);
+}
+
+function renderSerieDetail(series: Serie): void {
+    const cardContainer = document.getElementById("card-container");
+    if (!cardContainer) return;
+
+    cardContainer.innerHTML = "";
+
+    const cardHTML =`
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;>
+        <img src="${series.imageUrl}" class="card-img-top" alt="${series.name}">
+        <div class="card-body">
+            <h5 class="card-title">${series.name}</h5>
+            <p class="card-text">${series.description}</p>
+            <a href="${series.link}" class="btn btn-primary">Go watch it</a>
+        </div>
+    </div>
+    `;
+
+    cardContainer.innerHTML = cardHTML;
 }
